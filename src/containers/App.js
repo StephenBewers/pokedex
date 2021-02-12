053ac7c81ad75da0126d4a8bf8ce623bada59ada
phendev <stephen.bewers@gmail.com>
2021-02-12 20:01:15 +0000

    Implemented infinite scroll

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.scss";
 import PokemonCardList from "../components/PokemonCardList.js";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Pokedex = require("pokeapi-js-wrapper");
 const customOptions = {
@@ -12,28 +13,47 @@ class App extends Component {
     super();
     this.state = {
       pokemons: [],
+      count: 1,
+      hasMore: true,
     };
   }
 
-  componentDidMount() {
+  getPokemon = () => {
+    if (this.state.pokemons.length >= this.state.count) {
+      this.setState({ hasMore: false });
+      return;
+    }
     try {
       (async () => {
-        const interval = { limit: 151 };
+        const interval = { offset: this.state.pokemons.length, limit: 40 };
         let response = await PokeApi.getPokemonsList(interval);
+        this.setState({ count: response.count });
         let pokemonObjects = [];
         for (const item of response.results) {
           let pokemonObject = await PokeApi.resource(`${item.url}`);
           pokemonObjects.push(pokemonObject);
         }
-        this.setState({ pokemons: pokemonObjects });
+        this.setState({ pokemons: this.state.pokemons.concat(pokemonObjects) });
       })();
     } catch {
       console.error(`Failed to get Pokemon list`);
     }
+  };
+
+  componentDidMount() {
+    this.getPokemon();
   }
 
   render() {
-    return <PokemonCardList pokemons={this.state.pokemons} />;
+    return (
+      <InfiniteScroll
+        dataLength={this.state.pokemons.length}
+        next={this.getPokemon}
+        hasMore={this.state.hasMore}
+      >
+        <PokemonCardList pokemons={this.state.pokemons} />
+      </InfiniteScroll>
+    );
   }
 }
 
