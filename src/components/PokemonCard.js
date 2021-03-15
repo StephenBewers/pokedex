e816f0cc9e92467293a8f05a87805f5b8a26b163
phendev <stephen.bewers@gmail.com>
2021-03-15 21:03:03 +0000

    Add background image, style H1, and change card backgrounds to match new landing page style

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -6,21 +6,19 @@ const PokemonCard = ({ number, name, types, image }) => {
   const primaryTypeClass = `${types[0].type.name}-type`;
 
   // If the pokemon has a second type, get the second type class
-  const secondaryTypeClass = types.length > 1 ? `${types[1].type.name}` : "";
+  const secondaryTypeClass = types.length > 1 ? `${types[1].type.name}-secondary` : "";
 
   return (
     <Tilt
-      className={`pokemon-card ${primaryTypeClass}`}
+      className={`pokemon-card ${primaryTypeClass} ${secondaryTypeClass}`}
       perspective={500}
       glareEnable={true}
       glareMaxOpacity={0.45}
       scale={1.02}
     >
-      <div className={`secondary-type ${secondaryTypeClass}`}>
         <span className="pokemon-number">{number}</span>
         <img className="pokemon-img" src={image} alt={name} />
         <span className="pokemon-name">{name}</span>
-      </div>
     </Tilt>
   );
 };
